b9a8f025c52c6661c3e7a208ee2d3495808c97b3
Sunnycheck <ppalex.g@gmail.com>
2022-11-08 15:07:47 +0200

    Added new function

--- script.js
+++ script.js
@@ -84,8 +84,9 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-console.log('Hello');
-
+const newFeature = function () {
+  console.log('Hello');
+};
 // Coding Challenge #1
 
 /* 
